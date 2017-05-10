042d051d120c2b8e65b095cf1ce888c0d97feb8d
Sushrut Shringarputale <sush.shring@gmail.com>
2017-05-10 16:35:55 +0530

    made the script more secure

--- gitCollab.py
+++ gitCollab.py
@@ -1,24 +1,16 @@
-import sys, requests as r
+import sys, requests as r, getpass
 #This script takes 4 arguments to add a list of github usernames
 #and adds them as collaborators to a repository of your choice.
 #The format for the command is python gitCollab.py <username> <repo_name> <pathTo_file> <password>"
 
 
-try:
-    username = sys.argv[1]
-    repo = sys.argv[2]
-    fname = sys.argv[3]
-    addToRepo(username, repo, fname, sys.argv[4])
-except IndexError:
-    print "Not enough arguments provided"
 
-def addToRepo(username, repo, filename, password):
-    if len(sys.argv) == 1:
+def addToRepo(username, repo, filename):
+
+    if len(sys.argv) < 4:
+        print "Not enough arguments"
         print "Format is: python gitcollab.py <username> <repo_name> <pathTo_file> <password>"
         return
-
-    if len(sys.argv) < 5:
-        print "Not enough arguments"
         return
         pass
 
@@ -29,10 +21,23 @@ def addToRepo(username, repo, filename, password):
     content = [x.strip() for x in content]
 
     url = "https://api.github.com/repos/"
+    
+    password = getpass.getpass("Enter password for github user " + username)
 
     for name in content:
         print name
-        response = r.put(url+username+"/"+repo+"/collaborators/"+name, auth=(sys.argv[1],sys.argv[4]))
+        response = r.put(url+username+"/"+repo+"/collaborators/"+name, auth=(sys.argv[1],password))
         print response
 
+try:
+    username = sys.argv[1]
+    repo = sys.argv[2]
+    fname = sys.argv[3]
+    addToRepo(username, repo, fname)
+except IndexError:
+    print "Not enough arguments provided"
+    print """Requires four arguments:
+            1) github username <username>
+            2) github repository <repo>
+            3) text file with usernames to add as collaborators, separated by line."""
 
